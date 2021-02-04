bb76abe6e57bf838f11b99032f7628a7edcc96a7
jdwrhodes <jdw.rhodes@gmail.com>
2021-02-04 16:03:53 -0600

    Updated queries

--- queries.sql
+++ queries.sql
@@ -46,7 +46,7 @@ AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
 
 select * from retirement_info;
 
-drop table retirement_info;
+drop table emp_info;
 
 SELECT emp_no, first_name, last_name
 INTO retirement_info
@@ -115,3 +115,81 @@ FROM salaries as sa
 LEFT JOIN employees as em
 ON sa.emp_no = em.emp_no
 GROUP BY em.gender;
+
+
+SELECT e.emp_no, 
+	e.first_name, 
+	e.last_name, 
+	e.gender,
+	s.salary,
+	de.to_date
+INTO emp_info
+FROM employees as e
+INNER JOIN salaries as s
+ON (e.emp_no = s.emp_no)
+INNER JOIN dept_emp as de
+ON (e.emp_no = de.emp_no)
+WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (e.hire_date BETWEEN '1985-01-01' AND '1988-12-31')
+AND (de.to_date = '9999-01-01');
+
+
+select * from emp_info;
+
+
+-- List of Managers per department
+SELECT dm.dept_no, 
+	d.dept_name,
+	dm.emp_no,
+	ce.first_name, 
+	ce.last_name,
+	dm.from_date,
+	dm.to_date
+INTO manager_info
+FROM dept_manager as dm
+	INNER JOIN departments as d
+		ON (dm.dept_no = d.dept_no)
+	INNER JOIN current_emp as ce
+		ON (dm.emp_no = ce.emp_no);
+
+select * from manager_info;
+
+
+-- Department Retirees
+SELECT ce.emp_no,
+	ce.first_name,
+	ce.last_name,
+	d.dept_name
+INTO dept_info
+FROM current_emp as ce
+INNER JOIN dept_emp as de
+	ON (ce.emp_no = de.emp_no)
+INNER JOIN departments as d
+	ON (de.dept_no = d.dept_no);
+
+
+select * from dept_info;
+
+-- Sales team query using info from Retirement_Info table
+SELECT ri.emp_no,
+	ri.first_name,
+	ri.last_name,
+	d.dept_name
+FROM retirement_info as ri
+INNER JOIN dept_emp as de
+	ON (ri.emp_no = de.emp_no)
+INNER JOIN departments as d
+	ON (de.dept_no = d.dept_no)
+WHERE d.dept_name = 'Sales';
+
+-- Get both sales and Development
+SELECT ri.emp_no,
+	ri.first_name,
+	ri.last_name,
+	d.dept_name
+FROM retirement_info as ri
+INNER JOIN dept_emp as de
+	ON (ri.emp_no = de.emp_no)
+INNER JOIN departments as d
+	ON (de.dept_no = d.dept_no)
+WHERE d.dept_name IN('Sales', 'Development');
